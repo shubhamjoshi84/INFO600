5a977154448531f3efc69dc764fb3b492a059ef5
shubhamjoshi84 <56407582+shubhamjoshi84@users.noreply.github.com>
2019-10-17 14:48:42 -0400

    Create Week4_assignment1.js

---
+++ Week4_assignment1.js
@@ -0,0 +1,10 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    const button = document.querySelector('button');
+
+	button.addEventListener('click', event => {
+ //button.innerHTML = `Click count: ${event.detail}`;
+ changecolor("assgnmnt1");
+});
+});
+	
+
