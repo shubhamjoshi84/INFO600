8bca653a2067504a5558dd514a8a8eb6faa5550a
shubhamjoshi84 <56407582+shubhamjoshi84@users.noreply.github.com>
2019-11-03 19:05:27 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
 
 
-function addRec(){
+function addRecds(){
 	var name = document.getElementById("fname").value;	
 	var mjr = document.getElementById("major").value;
 	var stdyr = document.getElementById("year").value;
+	stdyr = parseInt(stdyr);
 							
 	
 	if (stdyr >= 2000){
-		var table = document.getElementById("tName");
+		var table = document.getElementById("enteredRecords");
 		var x = table.rows.length;
 		var row = table.insertRow(x);
 		var col = row.insertCell (0);
 		
 		var dte = new Date();
 		var time = dte.getHours()+ ":" + dte.getMinutes();
-		var addrecord = time + "-" + " " + name + " ," + mjr + ", " + stdyr ;  
+		var addrecord = time + " -" + " " + name + ", " + mjr + ", " + stdyr ;  
 		col.colSpan = "7";	
 		col.innerHTML = addrecord;					
 		document.getElementById("fname").value = "";	
@@ -22,5 +23,5 @@ function addRec(){
 		document.getElementById("year").value = "";					
 	
 	} else 
-		alert ("Enter the year greater or equal to 2000");
+		alert ("Incorrect year:" + stdyr);
 }
